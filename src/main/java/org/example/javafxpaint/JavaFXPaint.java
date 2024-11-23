896c6d2dac53f4a073f689b4fef7a81ee73fcf87
kcryptock <kcryptock@gmail.com>
2024-11-23 14:30:50 +0100

    preview half done

--- src/main/java/org/example/javafxpaint/JavaFXPaint.java
+++ src/main/java/org/example/javafxpaint/JavaFXPaint.java
@@ -6,6 +6,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -59,16 +60,60 @@ public class JavaFXPaint extends Application {
     public void start(Stage primaryStage) {
         StackPane root = new StackPane();
         Canvas myCanvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+        Canvas sandboxCanvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
         GraphicsContext draw = myCanvas.getGraphicsContext2D();
+        GraphicsContext sandboxDraw = sandboxCanvas.getGraphicsContext2D();
 
         myCanvas.setOnMouseDragged(e -> {
             newPosition.x = e.getX();
             newPosition.y = e.getY();
             if (e.getX() >= 0 && e.getX() < CANVAS_WIDTH && e.getY() >= 0 && e.getY() < CANVAS_HEIGHT) {}
 
+            WritableImage snap = sandboxCanvas.snapshot(null, null);
+            myCanvas.getGraphicsContext2D().drawImage(snap, 0, 0);
+
             if (currentMode == CurrentMode.Line) {
                 draw.lineTo(e.getX(), e.getY());
                 draw.stroke();
+            }
+
+            if (currentMode == CurrentMode.Line) {
+                draw.closePath();
+            }
+            else if(currentMode == CurrentMode.Circle) {
+                double radius = (Math.abs(newPosition.x - oldPosition.x) + Math.abs(newPosition.y - oldPosition.y)) / 2;
+
+                wrapPositions();
+
+                draw.fillOval(oldPosition.x, oldPosition.y, radius, radius);
+                draw.strokeOval(oldPosition.x, oldPosition.y, radius, radius);
+            }
+            else if(currentMode == CurrentMode.Rectangle) {
+                double width = Math.abs(newPosition.x - oldPosition.x);
+                double height = Math.abs(newPosition.y - oldPosition.y);
+
+                wrapPositions();
+
+                draw.fillRect(oldPosition.x, oldPosition.y, width, height);
+                draw.strokeRect(oldPosition.x, oldPosition.y, width, height);
+            }
+            else if(currentMode == CurrentMode.Triangle) {
+                double width = Math.abs(newPosition.x - oldPosition.x);
+                double height = Math.abs(newPosition.y - oldPosition.y);
+
+                wrapPositions();
+
+                double x1 = oldPosition.x + width / 2;
+                double y1 = oldPosition.y;
+
+                double x2 = oldPosition.x + width;
+                double y2 = oldPosition.y + height;
+
+                double x3 = oldPosition.x;
+                double y3 = oldPosition.y + height;
+
+                draw.fillPolygon(new double[]{x1, x2, x3}, new double[]{y1, y2, y3}, 3);
+                draw.strokePolygon(new double[]{x1, x2, x3}, new double[]{y1, y2, y3}, 3);
             }
         });
 
@@ -114,6 +159,9 @@ public class JavaFXPaint extends Application {
                 draw.fillPolygon(new double[]{x1, x2, x3}, new double[]{y1, y2, y3}, 3);
                 draw.strokePolygon(new double[]{x1, x2, x3}, new double[]{y1, y2, y3}, 3);
             }
+
+            WritableImage snap = myCanvas.snapshot(null, null);
+            sandboxCanvas.getGraphicsContext2D().drawImage(snap, 0, 0);
         });
 
         myCanvas.setOnMousePressed(e -> {
@@ -150,6 +198,7 @@ public class JavaFXPaint extends Application {
         Button clearButton = createButton.apply("Clear", CurrentMode.None);
         clearButton.setOnAction(e -> {
             draw.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+            sandboxDraw.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         });
 
         ColorPicker colorPicker = new ColorPicker();
@@ -160,7 +209,6 @@ public class JavaFXPaint extends Application {
         });
 
         vbox.getChildren().addAll(lineButton, circleButton, rectangleButton, triangleButton, noneButton, clearButton, colorPicker);
-
 
         HBox hbox = new HBox();
 
@@ -173,6 +221,7 @@ public class JavaFXPaint extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
     }
